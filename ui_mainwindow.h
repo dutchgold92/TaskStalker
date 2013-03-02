9c9650ca7378b867749d48ec3544c5326ee2f7fb
Jay <b00039579@student.itb.ie>
2013-03-02 13:48:25 +0000

    Removed old .png files

--- ui_mainwindow.h
+++ ui_mainwindow.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'mainwindow.ui'
 **
-** Created: Fri Mar 1 15:25:36 2013
+** Created: Sat Mar 2 13:30:41 2013
 **      by: Qt User Interface Compiler version 4.8.2
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
